c020120ec6cd78033f3de7fe737d4e68d1afebbb
Zarima Moon <zarima1991@yahoo.com>
2023-07-27 23:27:49 -0700

    first commit

---
+++ always-be-coding-countries/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AllCountries from "./components/AllCountries/AllCountries";
+import CountryInfo from "./components/CountryInfo/CountryInfo";
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <div className="container">
+          <h5>Around the world</h5>
+        </div>
+      </div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<AllCountries />} />
+          <Route path="/country/:countryName" element={<CountryInfo />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
